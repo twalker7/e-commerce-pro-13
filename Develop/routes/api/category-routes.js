5002c9099fa218323b729c47040f0ebddd06d788
Thomas Walker <thomasnwalker@yahoo.com>
2021-09-29 05:28:53 -0700

    category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -66,7 +66,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((categories) => res.status(200).json(categories))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+
 
 });
 
